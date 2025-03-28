8c1e91ab17d5e0e449945cd87a23287041704363
Shweta Savaliya <shwetasavaliya7@gmail.com>
2025-03-28 16:18:25 +0530

    -implement greeting component and UI responsive

---
+++ src/components/greeting/index.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
+import styles from "./Greeting.module.scss";
+
+interface IGreetingNameProps {
+  name?: string | null;
+}
+
+const GreetingName = ({ name = "Guest" }: IGreetingNameProps) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.greeterName}> Hello, {name}</div>
+      <div className={styles.massage}> Welcome to our application</div>
+    </div>
+  );
+};
+
+const Greeting = () => {
+  const [name, setName] = useState<string | null>(null);
+  const [inputValue, setInputValue] = useState<string>("");
+  const navigate = useNavigate();
+
+  const updateName = () => {
+    setName(inputValue);
+    setInputValue("");
+  };
+
+  return (
+    <div>
+      <div onClick={() => navigate("/")} className={styles.backLink}>
+        <KeyboardBackspaceOutlinedIcon />
+        <div> Back to Tasks</div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.title}>2.Greeting Component</div>
+        <div className={styles.description}>
+          Create a greeting component that takes a name as a prop and displays
+          personalized massage.
+        </div>
+        <GreetingName name={name || undefined} />
+        <div className={styles.greeterInput}>
+          <label className={styles.label}>Enter a name:</label>
+          <div className={styles.input}>
+            <input
+              className={styles.inputNum}
+              type="text"
+              value={inputValue}
+              placeholder="Enter your name"
+              onChange={(e) => setInputValue(e.target.value)}
+            ></input>
+            <button
+              onClick={updateName}
+              className={`${styles.updateBtn} ${
+                !inputValue ? styles.buttonDisable : ""
+              }`}
+            >
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Greeting;
--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -3,52 +3,62 @@ import { ITasks } from "../components/tasks";
 export const tasks: ITasks[] = [
   {
     id: 1,
-    title: "Counter",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    title: "Counter App",
+    description:
+      "Create a simple counter with increment,decrement and reset buttons using useState.",
   },
   {
     id: 2,
-    title: "Calculator",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    title: "Greeting Component",
+    description:
+      "Create a greeting component that takes a name as a prop and display personalized massage.",
   },
   {
     id: 3,
     title: "Counter",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 4,
     title: "Calculator",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 5,
     title: "Counter",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 6,
     title: "Calculator",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 7,
     title: "Counter",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 8,
     title: "Calculator",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 9,
     title: "Counter",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
   {
     id: 10,
     title: "Calculator",
-    description: "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
+    description:
+      "Count the value using a class component with Increment and Decrement,state methods, and updates the value using the setState method.",
   },
 ];
